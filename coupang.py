90a96b3d13c14b0f1d8ac41753f7ab78821bc985
David Jang <bbq12340@hotmail.com>
2020-09-06 23:55:04 +0900

    last commit

--- coupang.py
+++ coupang.py
@@ -10,47 +10,26 @@ from bs4 import BeautifulSoup
 import math, time, csv, json
 import pandas as pd
 
-<<<<<<< HEAD
 
 def scrape_product_page(browser, product_link):
     product_df = pd.read_csv('coupang.csv', encoding='utf-8')
-=======
-URL = 'https://www.coupang.com/np/search?q=%ED%81%AC%EB%A6%BC%EB%B9%B5&channel=user&component=&eventCategory=SRP&trcid=&traid=&sorter=scoreDesc&minPrice=&maxPrice=&priceRange=&filterType=&listSize=72&filter=&isPriceRange=false&brand=&offerCondition=&rating=0&page=2&rocketAll=false&searchIndexingToken=1=4&backgroundColor='
-
-
-def scrape_product_page(browser, product_link):
-    product_df = pd.DataFrame([], columns=['상품명', '가격', '평점', '리뷰','작성일', '링크']) #1개 상품 데이터 프레임 - 리뷰의 수에 따라 열 크기가 정해진다
->>>>>>> 3ecc937d58415cb2dde92d3a74e8d36e8e1300c2
     product_link = "https://www.coupang.com"+product_link
     try:
         browser.get(product_link)
     except TimeoutException:
         time.sleep(3)
         browser.refresh()
-<<<<<<< HEAD
     time.sleep(0.5)
     browser.execute_script("document.getElementsByName('review')[0].click();")
     time.sleep(1)
-=======
-    wait = WebDriverWait(browser, 30)
-    browser.execute_script('window.scrollTo(0, document.body.scrollHeight);')
-    review_by_class_name = By.CLASS_NAME, 'sdp-review'
-    wait.until(EC.presence_of_element_located(review_by_class_name))
->>>>>>> 3ecc937d58415cb2dde92d3a74e8d36e8e1300c2
     html = browser.execute_script('return document.body.outerHTML')
     soup = BeautifulSoup(html, 'html.parser')
     title = soup.find('h2', {'class': 'prod-buy-header__title'}).text
     print(f'\n현재 크롤링 중인 상품: {title}')
     price = soup.find('span', {'class': 'total-price'}).find('strong').text
-<<<<<<< HEAD
     review_count = soup.find('div', {'class': 'sdp-review__average__total-star__info-count'})
     #리뷰가 없는 경우
     if review_count == None:
-=======
-    no_review = soup.find('p', text='등록된 상품평이 없습니다.')
-    #리뷰가 없는 경우
-    if no_review:
->>>>>>> 3ecc937d58415cb2dde92d3a74e8d36e8e1300c2
         print('리뷰 수: 0')
         rate = None
         review = None
@@ -61,23 +40,16 @@ def scrape_product_page(browser, product_link):
             csv_writer.writerow(row)
     #리뷰가 있는 경우
     else:
-<<<<<<< HEAD
         review_count = int(review_count.text.replace(',',''))
         print(f'리뷰 수: {review_count}')
         while len(product_df.index) < review_count:
             time.sleep(0.5)
             html = browser.execute_script('return document.body.outerHTML')
             soup = BeautifulSoup(html, 'html.parser')
-=======
-        review_count = int(soup.find('div', {'class': 'sdp-review__average__total-star__info-count'}).text) #리뷰 수
-        print(f'리뷰 수: {review_count}')
-        while len(product_df.index) < review_count:
->>>>>>> 3ecc937d58415cb2dde92d3a74e8d36e8e1300c2
             tr = soup.find_all('article', {'class': 'sdp-review__article__list js_reviewArticleReviewList'})
             for r in tr:
                 rate = r.find('div', {'class': 'js_reviewArticleRatingValue'})['data-rating']
                 date = r.find('div', {'class': 'sdp-review__article__list__info__product-info__reg-date'}).text.replace('.','-')
-<<<<<<< HEAD
                 review = r.find('div', {'class': 'js_reviewArticleContent'})
                 if review:
                     review = review.text.replace('\n', ' ')
@@ -89,25 +61,12 @@ def scrape_product_page(browser, product_link):
                     csv_writer.writerow(row)
                 product_df = pd.read_csv('coupang.csv', encoding='utf-8')
                 product_df = product_df[product_df['상품명'] == title]
-=======
-                review = r.find('div', {'class': 'js_reviewArticleContent'}).text.replace('\n', ' ')
-                row = [title, price, rate, review, date, product_link]
-                row_df = pd.DataFrame([row], columns=list(product_df.columns))
-                product_df = product_df.append(row_df, ignore_index=True)
-                product_df.to_csv('coupang.csv')
->>>>>>> 3ecc937d58415cb2dde92d3a74e8d36e8e1300c2
                 if len(product_df.index) == review_count:
                     break
             #리뷰 다음 페이지 클릭
             try:
-<<<<<<< HEAD
                 browser.execute_script("document.querySelector('.sdp-review__article__page__num--active').nextElementSibling.click();")
             except JavascriptException:
-=======
-                review_next_btn = browser.find_element_by_class_name('js_reviewArticlePageNextBtn')
-                review_next_btn.click()
-            except NoSuchElementException:
->>>>>>> 3ecc937d58415cb2dde92d3a74e8d36e8e1300c2
                 pass
     return
 
@@ -174,13 +133,7 @@ def open_browser(url):
 def extract_coupang(url):
     LIMITED_ITEMS = 1000
     browser = open_browser(url)
-<<<<<<< HEAD
     pagination(browser, LIMITED_ITEMS)
     browser.quit()    
     df = pd.read_csv('coupang.csv', encoding='utf-8', index_col=0)
     return df
-=======
-    pagination(browser, LIMITED_ITEMS)    
-
-extract_coupang(URL)
->>>>>>> 3ecc937d58415cb2dde92d3a74e8d36e8e1300c2
